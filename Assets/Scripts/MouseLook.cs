4116d148e7e54898de67324b9ed3073a50aa689b
pierce12661 <pierce12661@gmail.com>
2023-02-27 23:29:20 +0800

    Level
    - Movement (DONE)
    - Jumping (DONE)
    - Looking Around (DONE)

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 300f;
+    public Transform playerBody;
+
+    public bool flipXRotation = false;
+    public bool flipYRotation = false;
+    public float yLookMin = -80f;
+    public float yLookMax = 80f;
+
+    private float xRotation = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        //Vertical Look
+        xRotation += flipYRotation ? mouseY : -mouseY;
+        xRotation = Mathf.Clamp(xRotation, yLookMin, yLookMax);
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+
+        //Horizontal Look
+        playerBody.Rotate(Vector3.up, flipXRotation ? -mouseX : mouseX);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 4f;
+
+    public float gravity = -9.81f;
+    public float jumpHeight = 0.03f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.1f;
+    public LayerMask groundMask; // checking if surface layer is ground
+    public float groundStop = -0.1f;
+
+    private Vector3 velocity;
+    private bool isGrounded = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+                
+        controller.Move(Vector3.ClampMagnitude(move, 1f) * speed * Time.deltaTime);
+
+        //Gravity
+        velocity.y += gravity * Time.deltaTime * Time.deltaTime;
+
+        //Ground Check
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = groundStop;
+        }
+
+        //Jump
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (isGrounded)
+            {
+                //Cancel gravity so can jump
+                velocity.y = jumpHeight;
+            }
+            
+        }
+
+        controller.Move(velocity);
+    }
+}
