3e9d198cb4af4feb50adaac8d37a4bf14ff0caf5
CenturionLynx <markwong98@hotmail.com>
2023-03-16 16:17:01 +0800

    Grenade
    Created a Grenade with explosion effects

---
+++ Assets/Scripts/Grenade.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+
+    public float delay = 3f;
+    public float radius = 5f;
+    public float force = 700f;
+
+    public GameObject explosionEffect; 
+
+    float countdown; 
+    bool  hasExploded = false; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdown = delay; //coundown = 3f sec before exploding
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+        if(countdown <= 0f && !hasExploded)
+        {
+            Explode();
+            hasExploded = true;
+        }
+    }
+
+    void Explode()
+    {
+        Debug.Log("Boom");
+
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+
+        //Detect nearby objects 
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius); 
+
+        foreach (Collider nearbyObject in colliders)
+        {
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(force, transform.position, radius);
+            }
+        }
+        // Add force 
+
+        // Damage them 
+
+
+        Destroy(gameObject);
+        
+    }
+}
